bc7cfdcd676feac573d16dbc4376f86ea2dde6c4
Pawel Lisewski <pawel.lisewski@gmail.com>
2018-01-28 16:56:16 +0800

    install spectaclejs

---
+++ node_modules/spectacle/src/components/code-pane.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { getStyles } from '../utils/base';
+import styled from 'react-emotion';
+
+import '../utils/prism-import';
+import { Editor } from 'react-live';
+
+const StyledWrapper = styled.div(props => props.styles);
+const StyledEditor = styled(({ syntaxStyles: _, prismTheme: __, ...rest }) => <Editor {...rest} />)`
+  && {
+    ${props => props.syntaxStyles}
+  }
+
+  ${props => props.prismTheme}
+`;
+
+export default class CodePane extends Component {
+  handleEditorEvent(evt) {
+    evt.stopPropagation();
+  }
+
+  render() {
+    const useDarkTheme = this.props.theme === 'dark';
+
+    const wrapperStyles = [
+      this.context.styles.components.codePane,
+      getStyles.call(this),
+      this.props.style
+    ];
+
+    return (
+      <StyledWrapper
+        className={this.props.className}
+        styles={wrapperStyles}
+      >
+        <StyledEditor
+          className="language-prism"
+          code={this.props.source}
+          language={this.props.lang}
+          contentEditable={this.props.contentEditable}
+          syntaxStyles={this.context.styles.components.syntax}
+          prismTheme={this.context.styles.prism[useDarkTheme ? 'dark' : 'light']}
+          onKeyDown={this.handleEditorEvent}
+          onKeyUp={this.handleEditorEvent}
+          onClick={this.handleEditorEvent}
+        />
+      </StyledWrapper>
+    );
+  }
+}
+
+CodePane.contextTypes = {
+  styles: PropTypes.object,
+  store: PropTypes.object
+};
+
+CodePane.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  contentEditable: PropTypes.bool,
+  lang: PropTypes.string,
+  source: PropTypes.string,
+  style: PropTypes.object,
+  theme: PropTypes.string,
+};
+
+CodePane.defaultProps = {
+  theme: 'dark',
+  className: '',
+  contentEditable: false,
+  lang: 'markup',
+  source: '',
+};
