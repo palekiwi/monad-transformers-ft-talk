7a4dd9526d27f5edcca7902dd74d1adc2c6a9274
Pawel Lisewski <pawel.lisewski@gmail.com>
2018-02-07 00:38:54 +0800

    add more slides

--- src/presentation.js
+++ src/presentation.js
@@ -15,6 +15,7 @@ import {
   Slide,
   Text,
   CodePane,
+  S,
   Table,
   TableHeader,
   TableRow,
@@ -34,7 +35,7 @@ const theme = createTheme(
     primary: 'white',
     secondary: '#1F2022',
     tertiary: '#03A9FC',
-    quartenary: '#CECECE',
+    quartenary: '#999',
   },
   {
     primary: 'Montserrat',
@@ -43,9 +44,9 @@ const theme = createTheme(
 );
 
 const cs = {
-  inOut: {bg: 'tertiary', h: 'primary', t: 'secondary', p: 'primary'},
-  p1: {bg: 'primary', h: 'tertiary', t: 'secondary', p: 'tertiary'},
-  p2 : {bg: 'quartenary', h: 'tertiary', t: 'secondary', p: 'tertiary'},
+  inOut: {bg: 'primary', h: 'secondary', t: 'tertiary', p: 'quartenary'},
+  p1: {bg: 'secondary', h: 'tertiary', t: 'primary', p: 'quartenary'},
+  p2: {bg: 'primary', h: 'secondary', t: 'tertiary', p: 'quartenary'},
   p3 : {bg: 'secondary', h: 'tertiary', t: 'primary', p: 'secondary'},
 };
 
@@ -90,12 +91,39 @@ export default class Presentation extends React.Component {
             Monad Transformers
           </Heading>
           <Text margin="10px 0 0" textColor={cs.inOut.t} size={5} bold>
-            by Pawel Lisewski
+            by 黎勇禪 Pawel Lisewski
           </Text>
         </Slide>
 
         <Slide bgColor={cs.inOut.bg} textColor={cs.inOut.t}>
+          <Heading fit size={4} caps lineHeight={1} textColor={cs.inOut.h}>
+            Why talk about Monad Transformers?
+          </Heading>
+          <Text margin="10px 0 0" textColor={cs.inOut.t} size={5} bold>
+            Is this healthy?
+          </Text>
+        </Slide>
+
+        <Slide bgColor={cs.inOut.bg} textColor={cs.inOut.t}>
+          <Heading margin="10px 0 0" textColor={cs.inOut.t} size={5} bold>
+            But why bother?
+          </Heading>
           <Image src={require("./assets/img/luke-obiwan-peering-into-lightsaber.jpg")} />
+        </Slide>
+
+        <Slide bgColor={cs.inOut.bg} textColor={cs.inOut.t}>
+          <Heading size={4} textAlign="left" lineHeight={1} textColor={cs.inOut.t}>
+            do
+          </Heading>
+          <Heading size={4} textAlign="left" caps lineHeight={1} textColor={cs.inOut.h}>
+            Part I: The Base >>=
+          </Heading>
+          <Heading size={4} textAlign="left" caps lineHeight={1} textColor={cs.inOut.h}>
+            Part II: The Discovery >>=
+          </Heading>
+          <Heading size={4} textAlign="left" caps lineHeight={1} textColor={cs.inOut.h}>
+            Part III: The Journey
+          </Heading>
         </Slide>
 
         { /* Part I */ }
@@ -110,15 +138,106 @@ export default class Presentation extends React.Component {
 
         <Slide bgColor={cs.p1.bg} textColor={cs.p1.t}>
           <BlockQuote>
-            <Quote textColor={cs.p1.h}>The voyage of discovery is not in seeking new landscapes but in having new eyes.</Quote>
+            <Quote textColor={cs.p1.h}>The true voyage of discovery is not in seeking new landscapes but in having new eyes.</Quote>
             <Cite textColor={cs.p1.t}>Marcel Proust</Cite>
           </BlockQuote>
+        </Slide>
+
+        <Slide bgColor={cs.p1.bg} textColor={cs.p1.t}>
+          <Heading fit size={3} textColor={cs.p1.t} caps>
+            A few thoughts about monads:
+          </Heading>
+          <List>
+            <ListItem>very common in modern FP code</ListItem>
+            <ListItem>many implementations, e.g. Maybe, Reader, Writer, STM...</ListItem>
+            <ListItem>specialized to solve very specific kinds of problems by providing an environment and services</ListItem>
+            <ListItem>...</ListItem>
+          </List>
+        </Slide>
+
+        <Slide bgColor={cs.p1.bg} textColor={cs.p1.t}>
+          <Heading fit size={3} textColor={cs.p1.t} caps>
+            State Monad: environment and services
+          </Heading>
+          <Text fit margin="10px 0 0" textColor={cs.inOut.t} size={5} bold>
+            (Store the value for later reference, query the value)
+          </Text>
+          <Haskell source={require('./assets/StateEnvServices.hs')} />
+        </Slide>
+
+        <Slide bgColor={cs.p1.bg} textColor={cs.p1.t}>
+          <Heading fit size={3} textColor={cs.p1.t} caps>
+            The Power Of Monad:
+          </Heading>
+          <List>
+            <ListItem><s strikethrough>makes pure state possible</s></ListItem>
+            <ListItem><s strikethrough>makes side effects possible</s></ListItem>
+            <ListItem><s strikethrough>makes burritos possible</s></ListItem>
+            <ListItem>makes composition possible</ListItem>
+          </List>
+          <Text margin="10px 0 0" textColor={cs.inOut.t} size={5} bold>
+          </Text>
+          <Haskell source={require('./assets/MonadComposition.hs')} />
+        </Slide>
+
+        <Slide bgColor={cs.p1.bg} textColor={cs.p1.t}>
+          <Heading fit size={3} textColor={cs.p1.t} caps>
+            The Power Of Monad:
+          </Heading>
+          <List>
+            <ListItem>makes composition possible</ListItem>
+          </List>
+          <Text fit textColor={cs.inOut.t} size={6} bold>
+            Monad takes care of threading the stored value through the whole computation
+          </Text>
+          <Text margin="10px 0 0" textColor={cs.inOut.t} size={5} bold>
+          </Text>
+          <Haskell source={require('./assets/MonadComposition.hs')} />
+        </Slide>
+
+        <Slide bgColor={cs.p1.bg} textColor={cs.p1.t}>
+          <Heading fit size={3} textColor={cs.p1.t} caps>
+            In other words, formally:
+          </Heading>
+          <Text margin="10px 0 0" textColor={cs.inOut.t} size={5} bold>
+            Monads promote modularity and code reuse by encapsulating often-used computational strategies into single blocks of code that can be used to construct many different computations.
+          </Text>
+        </Slide>
+
+        <Slide bgColor={cs.p1.bg} textColor={cs.p1.t}>
+          <Heading fit size={3} textColor={cs.p1.t} caps>
+            Meanwhile in the real world... it's layers and layers:
+          </Heading>
+          <List>
+            <ListItem>talking to DBs</ListItem>
+            <ListItem>using upstream APIs</ListItem>
+            <ListItem>own business logic with configs and exceptions</ListItem>
+            <ListItem>...</ListItem>
+          </List>
+        </Slide>
+
+        <Slide bgColor={cs.p1.bg} textColor={cs.p1.t}>
+          <Heading fit size={3} textColor={cs.p1.t} caps>
+            Question:
+          </Heading>
+          <Text margin="10px 0 0" textColor={cs.inOut.t} size={5}>
+            What happens when we need to use different 'monadic services' at the same time?
+          </Text>
         </Slide>
 
         { /* Part II */ }
         <Slide bgColor={cs.p2.bg}>
           <Heading size={3} textColor={cs.p2.t} caps>
             Part II
+          </Heading>
+          <Heading size={1} textColor={cs.p2.h} caps fit>
+            The Discovery
+          </Heading>
+        </Slide>
+
+        <Slide bgColor={cs.p2.bg}>
+          <Heading size={3} textColor={cs.p2.t} caps>
+            Example: Login shell
           </Heading>
           <Heading size={1} textColor={cs.p2.h} caps fit>
             The Discovery
